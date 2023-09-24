80c9cd98e3019440ca8b748d96e3e450e405ac8d
Brian D <contact@noimbrian.com>
2023-09-23 21:50:07 -0500

    feat: create database for platform

--- platform/src/Platform.ts
+++ platform/src/Platform.ts
@@ -1,13 +1,50 @@
 import * as pulumi from "@pulumi/pulumi";
-import { buildUrn, URN_TYPE } from "@sprocketbot/infra-lib";
+import {
+  buildUrn,
+  TimescaleDatabase,
+  TimescaleDatabaseArgs,
+  URN_TYPE,
+} from "@sprocketbot/infra-lib";
+
+export type PlatformArgs = {
+  vaultConnName: TimescaleDatabaseArgs["vaultConnName"];
+  // TODO: Accept Redis
+  // TODO: Accept RMQ
+};
 
 export class Platform extends pulumi.ComponentResource {
-  constructor(name: string, args: {}, opts?: pulumi.ComponentResourceOptions) {
+  constructor(
+    name: string,
+    args: PlatformArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
     super(
       buildUrn(URN_TYPE.LogicalGroup, "SprocketPlatform", name),
       name,
       {},
       opts,
+    );
+
+    const database = new TimescaleDatabase(
+      "platform-db",
+      {
+        name: `sprocket_${pulumi.getStack()}`,
+        restrictedRoleAlias: "data_science",
+        schemas: {
+          data_science: { restrictedPerms: "rw" },
+          mledb_bridge: { restrictedPerms: "r" },
+          sprocket: { restrictedPerms: "r" },
+          history: { restrictedPerms: "r" },
+          mledb: { restrictedPerms: "r" },
+        },
+        searchPath: {
+          restricted: "sprocket,mledb,mledb_bridge,history,data_science",
+          write: "sprocket,mledb,mledb_bridge,history,data_science",
+        },
+        vaultConnName: args.vaultConnName,
+        static: { write: true },
+      },
+      { parent: this },
     );
   }
 }
--- platform/src/index.ts
+++ platform/src/index.ts
@@ -1,1 +1,29 @@
+import { Platform } from "./Platform";
+import {
+  getGrafanaProvider,
+  getPostgresProvider,
+  getVaultProvider,
+  InfrastructureStackOutputs,
+  InfrastructureStackRef,
+} from "@sprocketbot/infra-lib";
+
+if (InfrastructureStackRef === null)
+  throw new Error("InfrastructureStackRef is null");
+
+const p = new Platform(
+  "platform",
+  {
+    vaultConnName: InfrastructureStackRef.getOutput(
+      InfrastructureStackOutputs.PostgresVaultConnectionName,
+    ),
+  },
+  {
+    providers: [
+      getVaultProvider(),
+      getPostgresProvider(),
+      getGrafanaProvider(),
+    ],
+  },
+);
+
 export const X = "";
