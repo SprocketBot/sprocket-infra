210335806171a881c0136d01e728c9327c5fedec
Jake Bailey <asaxplayinghorse@gmail.com>
2025-09-21 13:31:51 -0700

    feat: all tokens working now. Vault should be good!

--- global/services/vault/scripts/auto-initialize.sh
+++ global/services/vault/scripts/auto-initialize.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-apk add curl jq;
-mkdir -p ../unseal-tokens
+apk update && apk add curl jq;
 
 export VAULT_ADDR=http://localhost:8200
 vault server --config /vault.hcl & echo "Server Starting";
@@ -16,7 +15,7 @@ echo "Sealed: $sealed"
 echo "Initialized: $initialized"
 
 if [ "$sealed" = "true" ]; then
-  if [ "$initialized" = "false" ]; then
+  if [ "$initialized" = "false" ] || [ ! -f /vault/unseal-tokens/unseal_tokens.txt ]; then
     echo "Initializing Vault"
     init_output=$(vault operator init 2>&1)
     exit_code=$?
@@ -25,10 +24,10 @@ if [ "$sealed" = "true" ]; then
       exit 1
     fi
     echo "Init output: $init_output"
-    echo "$init_output" | grep 'Unseal Key' | sed 's/.*: //' > ../unseal-tokens/unseal_tokens.txt
-    echo "$init_output" | grep 'Initial Root Token' | sed 's/.*: //' > ../unseal-tokens/root_token.txt
-    cat ../unseal-tokens/root_token.txt
-    cat ../unseal-tokens/unseal_tokens.txt | while read -r t; do
+    echo "$init_output" | grep 'Unseal Key' | sed 's/.*: //' > /vault/unseal-tokens/unseal_tokens.txt
+    echo "$init_output" | grep 'Initial Root Token' | sed 's/.*: //' > /vault/unseal-tokens/root_token.txt
+    cat /vault/unseal-tokens/root_token.txt
+    cat /vault/unseal-tokens/unseal_tokens.txt | while read -r t; do
       echo "Unsealing with key: $t"
       vault operator unseal "$t"
       sealed=$(vault status -format=json | jq -r '.sealed')
@@ -37,9 +36,9 @@ if [ "$sealed" = "true" ]; then
       fi
     done
   else
-    if [ -f ../unseal-tokens/unseal_tokens.txt ]; then
+    if [ -f /vault/unseal-tokens/unseal_tokens.txt ]; then
       echo "Vault is initialized but sealed. Unsealing..."
-      cat ../unseal-tokens/unseal_tokens.txt | while read -r t; do
+      cat /vault/unseal-tokens/unseal_tokens.txt | while read -r t; do
         echo "Unsealing with key: $t"
         vault operator unseal "$t"
         sealed=$(vault status -format=json | jq -r '.sealed')
